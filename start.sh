3f89ebded0ebd13958d90d3fe9216c9a82c1a69f
Adam Dodman <Adam-Ant@users.noreply.github.com>
2016-09-14 00:06:03 +0100

    Path fixes for media-compose.yml

--- start.sh
+++ start.sh
@@ -28,6 +28,6 @@ for ((i=0; i<$slen; i++)); do
  [[ $( ls -dn $VOLDIR/${SERVICES[$i]} | awk '{print $3}') != ${SERVICEUID[$i]} ]] && echo "Chowning $VOLDIR/${SERVICES[$i]} to user ${SERVICEUID[$i]}" && chown ${SERVICEUID[$i]}:${SERVICEUID[$i]} $VOLDIR/${SERVICES[$i]}
 done
 
-[[ ! -a $VOLDIR/launcher/docker-compose.yml ]] && echo "Downloading docker-compose.yml.." && curl -sSL https://raw.githubusercontent.com/Adam-Ant/media-server-in-a-box/master/docker-compose.yml > $VOLDIR/launcher/media-compose.yml
+[[ ! -a $VOLDIR/launcher/media-compose.yml ]] && echo "Downloading media-compose.yml.." && curl -sSL https://raw.githubusercontent.com/Adam-Ant/media-server-in-a-box/master/docker-compose.yml > $VOLDIR/launcher/media-compose.yml
 echo "Starting services..."
 exec docker-compose -p media -f $VOLDIR/launcher/media-compose.yml up -d
